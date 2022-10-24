b286d6b6acc04364343a7ebda86e3cd187c90d83
sam_dev <samueldevpro09@gmail.com>
2022-10-24 04:10:05 +0200

    removing profile image featur

--- src/components/settings/ProfilePictures.jsx
+++ src/components/settings/ProfilePictures.jsx
@@ -7,9 +7,11 @@ import UserContext from "../../context/UserContext";
 const ProfilePictures = () => {
   const { login, changeLogin } = useContext(UserContext);
   const [uploading, setUploading] = useState(false);
+  const [removing, setRemoving] = useState(false);
   const [need, setNeed] = useState([]);
   const [type, setType] = useState("");
   const [cloudResult, setCloudResult] = useState(null);
+  const [removeData, setRemoveData] = useState(null);
 
   const cloudinary = useRef();
   cloudinary.current = window.cloudinary;
@@ -25,6 +27,7 @@ const ProfilePictures = () => {
         cloudName: cloudName,
         uploadPreset: uploadPreset,
         cropping: true, //add a cropping step
+        folder: "profiles",
         sources: ["local"], // restrict the upload sources to URL and local files
         multiple: false, //restrict upload to a single file
         clientAllowedFormats: ["jpg", "jpeg", "png"], //restrict uploading to image files only
@@ -61,6 +64,12 @@ const ProfilePictures = () => {
       notifLoading("uploading...");
     }
   }, [uploading]);
+
+  useEffect(() => {
+    if (removing) {
+      notifLoading("removing...");
+    }
+  }, [removing]);
 
   // creating widget on comp mount
   useEffect(() => {
@@ -124,6 +133,80 @@ const ProfilePictures = () => {
     sendUpdate();
   }, [cloudResult]);
 
+  // remove files
+  const removeIcon = () => {
+    setType("icon");
+    setRemoveData({
+      default:
+        "https://res.cloudinary.com/dm7pcraut/image/upload/v1666499517/u_profile_main/1946429_dpitlh.png",
+      thumb:
+        "https://res.cloudinary.com/dm7pcraut/image/upload/c_limit,h_60,w_90/v1666499517/u_profile_main/1946429_dpitlh.png",
+    });
+  };
+
+  const removeBack = () => {
+    setType("back");
+    setRemoveData({
+      default:
+        "https://res.cloudinary.com/dm7pcraut/image/upload/v1666500653/u_profile_main/dvux_fyhbql.png",
+      thumb:
+        "https://res.cloudinary.com/dm7pcraut/image/upload/c_limit,h_60,w_90/v1666499517/u_profile_main/1946429_dpitlh.png",
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (removeData) {
+        setRemoving(true);
+        setNeed([]);
+        // sending file to server
+        const data = {
+          uid: login.user.id,
+          imageType: type,
+          imageUri: removeData.default,
+          imageUriSmall: removeData.thumb,
+        };
+
+        // send update to server
+        try {
+          let headers = new Headers();
+          headers.append("Content-Type", "application/json");
+          headers.append("Accept", "application/json");
+          headers.append("GET", "POST", "OPTIONS");
+          headers.append(
+            "Access-Control-Allow-Origin",
+            `${process.env.REACT_APP_DOMAIN}`
+          );
+          headers.append("Access-Control-Allow-Credentials", "true");
+
+          const response = await fetch(
+            `${process.env.REACT_APP_DOMAIN}/twitter/api/settings/profileimg`,
+            {
+              mode: "cors",
+              method: "POST",
+              headers: headers,
+              body: JSON.stringify(data),
+              credentials: "include",
+            }
+          );
+
+          const serverMessage = await response.json();
+          setUploading(false);
+          notif(serverMessage.message);
+
+          // update the component
+          setRemoving(false);
+          setNeed(["now"]);
+
+          // reload component
+        } catch (err) {
+          notif("server error try again later");
+          console.log(err);
+        }
+      }
+    })();
+  }, [removeData]);
+
   return (
     <>
       {need.map((elm, idx) => (
@@ -158,7 +241,7 @@ const ProfilePictures = () => {
                 Change
               </label>
             </form>
-            <button>Remove</button>
+            <button onClick={removeIcon}>Remove</button>
           </div>
         </div>
 
@@ -184,7 +267,7 @@ const ProfilePictures = () => {
                 Change
               </label>
             </form>
-            <button>Remove</button>
+            <button onClick={removeBack}>Remove</button>
           </div>
         </div>
       </div>
