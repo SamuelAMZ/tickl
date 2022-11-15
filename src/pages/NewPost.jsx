78227d9891a1c81d5c1799d348d31266ba254bee
sam_dev <samueldevpro09@gmail.com>
2022-11-15 14:24:00 +0100

    phone new posts

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Checks from "../components/Checks";
 import Loading from "../components/Loading";
-import { Link } from "react-router-dom";
+import notif from "../helpers/notif";
+import { Link, useNavigate } from "react-router-dom";
 import {
   BsImage,
   BsFillCameraVideoFill,
@@ -13,6 +14,100 @@ import UserContext from "../context/UserContext";
 
 const NewPost = () => {
   const { login, changeLogin } = useContext(UserContext);
+  const [newPostText, setNewPostText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  function OnInput(e) {
+    this.style.height = 0;
+    this.style.height = this.scrollHeight + "px";
+  }
+
+  useEffect(() => {
+    // auto scall textarea height
+    const txHeight = 47;
+    const tx = document.getElementsByTagName("textarea");
+
+    for (let i = 0; i < tx.length; i++) {
+      if (tx[i].value == "") {
+        tx[i].setAttribute(
+          "style",
+          "height:" + txHeight + "px;overflow-y:hidden;"
+        );
+      } else {
+        tx[i].setAttribute(
+          "style",
+          "height:" + tx[i].scrollHeight + "px;overflow-y:hidden;"
+        );
+      }
+      tx[i].addEventListener("input", OnInput, false);
+    }
+  }, []);
+
+  // post new tickl request
+  const newPost = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    if (!newPostText) {
+      setIsLoading(false);
+      return notif("Verify the post field");
+    }
+
+    // getting data
+    const data = {
+      ownerId: login.user.id,
+      newPostText,
+    };
+
+    // sending request
+    try {
+      let headers = new Headers();
+      headers.append("Content-Type", "application/json");
+      headers.append("Accept", "application/json");
+      headers.append("GET", "POST", "OPTIONS");
+      headers.append(
+        "Access-Control-Allow-Origin",
+        `${process.env.REACT_APP_DOMAIN}`
+      );
+      headers.append("Access-Control-Allow-Credentials", "true");
+
+      const response = await fetch(
+        `${process.env.REACT_APP_DOMAIN}/twitter/api/post/newpost`,
+        {
+          mode: "cors",
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify(data),
+          credentials: "include",
+        }
+      );
+
+      const serverMessage = await response.json();
+
+      setIsLoading(false);
+
+      if (serverMessage.code === "500") {
+        notif(serverMessage.error.message);
+        console.log(serverMessage.error.message);
+      }
+
+      if (serverMessage.code === "ok") {
+        // reset form
+        setNewPostText("");
+        // success message
+        notif(serverMessage.message);
+        // redirect to home
+        navigate("/home");
+      }
+    } catch (err) {
+      notif("server error try again later");
+      console.log(err);
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <SingleHeader title={"New Post"} />
@@ -33,13 +128,15 @@ const NewPost = () => {
                             alt="profile icon"
                           />
                         </Link>
-                        <form>
+                        <form onSubmit={newPost}>
                           <div className="top-elms">
                             <textarea
                               rows="1"
                               className="w-full text-lg tickltextarea"
                               placeholder="What's happening?"
                               autoFocus
+                              value={newPostText}
+                              onChange={(e) => setNewPostText(e.target.value)}
                             ></textarea>
                             <div className="top-elms-container">
                               <div>
@@ -49,12 +146,15 @@ const NewPost = () => {
                                 <RiFileGifFill />
                               </div>
                               <div>
-                                <button
-                                  className="btn btn-sm btn-primary capitalize"
-                                  disabled
-                                >
-                                  Tickl
-                                </button>
+                                {isLoading ? (
+                                  <button className="btn btn-sm btn-primary capitalize loading">
+                                    Posting...
+                                  </button>
+                                ) : (
+                                  <button className="btn btn-sm btn-primary capitalize">
+                                    Tickl
+                                  </button>
+                                )}
                               </div>
                             </div>
                           </div>
